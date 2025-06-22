a1761c81cd024b6f35dc448674ab14165a201f17
mucahityuksel <m.yuksel793@gmail.com>
2025-06-22 23:15:57 +0300

    -edited: types changed

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,53 +1,65 @@
-export type ModalProps = {
-    isOpen: boolean;
-    onClose: () => void;
-    historyData: any[];
-    getData: (data: any) => void;
+export type ExtendedDataItem = {
+  type: 'metric' | 'imperial';
+  city: {
+    country: string;
+    name: string;
+    sunrise: number;
+    sunset: number;
+  };
+  cnt: number;
+  cod: string;
+  list: ListType[];
+  message: string;
 };
 
+export type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  historyData: ExtendedDataItem[];
+  getData: (data: ExtendedDataItem) => void;
+};
+
+
 export type DataItem = {
-    city: {
-        country: string,
-        name: string,
-        sunrise: number,
-        sunset: number
-    };
-    cnt: number;
-    code: string;
-    list: ListType[];
-    message: number,
-    type: string
-}
+  city: {
+    country: string;
+    name: string;
+    sunrise: number;
+    sunset: number;
+  };
+  cnt: number;
+  cod: string;
+  list: ListType[];
+  message: string;
+};
 
 export type ListType = {
-    clouds: any,
-    dt: number,
-    dt_txt: string,
-    main: {
-        feels_like: number,
-        grnd_level: number,
-        humidity: number,
-        pressure: number,
-        sea_level: number,
-        temp: number,
-        temp_kf: number,
-        temp_max: number,
-        temp_min: number
-    },
-    pop: number,
-    sys: {
-        pod: string
-    },
-    weather: [
-        {
-            description: string,
-        }
-    ],
-    wind: {
-        deg: number,
-        gust: number,
-        speed: number
-    }
-}
-
-export type ExtendedDataItem = DataItem & { type: 'metric' | 'imperial' };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  dt_txt: string;
+  main: {
+    feels_like: number;
+    grnd_level: number;
+    humidity: number;
+    pressure: number;
+    sea_level: number;
+    temp: number;
+    temp_kf: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  weather: {
+    description: string;
+  }[];
+  wind: {
+    deg: number;
+    gust: number;
+    speed: number;
+  };
+};
